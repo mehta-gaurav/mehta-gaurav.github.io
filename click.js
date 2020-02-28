7606214ab06dc56c0dded8122a2c042ca20f89b6
murli1201 <54397265+murli1201@users.noreply.github.com>
2020-02-28 15:48:33 +0530

    Update click.js

--- click.js
+++ click.js
@@ -1,4 +1,14 @@
-$('li a').click(function(){
-    $('li a').removeClass("active");
-    $(this).addClass("active");
-});
+// $('li a').click(function(){
+//     $('li a').removeClass("active");
+//     $(this).addClass("active");
+// });
+
+
+ jQuery(function() {
+    var pgurl = window.location.href.substr(window.location.href
+      .lastIndexOf("/")+1);
+    jQuery(" ul li a").each(function(){
+      if(jQuery(this).attr("href") == pgurl || jQuery(this).attr("href") == '' )
+        jQuery(this).addClass("active");
+    })
+  });
